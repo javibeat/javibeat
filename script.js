7798e1e4296e46d1ae158688796cc57822dd7b7d
javibeat <javibeat@gmail.com>
2023-06-29 16:50:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,69 @@
 // Wait for the DOMContentLoaded event before executing the JavaScript code
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the button
-    let mybutton = document.getElementById("myBtn");
-  
-    // Add scroll event listener to the window
-    window.addEventListener("scroll", scrollFunction);
-  
-    function scrollFunction() {
-      if (window.scrollY > 20) {
-        mybutton.style.display = "block";
+  // Get all the menu links
+  const menuLinks = document.querySelectorAll('.nav-links a');
+
+  // Attach click event listeners to each menu link
+  menuLinks.forEach(link => {
+    link.addEventListener("click", scrollToSection);
+  });
+
+  // Add scroll event listener to the window
+  window.addEventListener("scroll", highlightCurrentSection);
+
+  // Smooth scroll to the target section
+  function scrollToSection(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetSection = document.querySelector(targetId);
+    const targetPosition = targetSection.offsetTop;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth"
+    });
+
+    // Close the mobile menu if open
+    const checkboxToggle = document.getElementById("checkbox_toggle");
+    checkboxToggle.checked = false;
+  }
+
+  // Highlight the current section in the menu
+  function highlightCurrentSection() {
+    const sections = document.querySelectorAll("section");
+    const scrollPosition = window.scrollY;
+
+    sections.forEach(section => {
+      const sectionId = section.getAttribute("id");
+      const navLink = document.querySelector(`.nav-links a[href="#${sectionId}"]`);
+
+      if (section.offsetTop <= scrollPosition + 100 && section.offsetTop + section.offsetHeight > scrollPosition + 100) {
+        navLink.classList.add("active");
       } else {
-        mybutton.style.display = "none";
+        navLink.classList.remove("active");
       }
+    });
+  }
+
+  // Back to top button functionality
+  let mybutton = document.getElementById("myBtn");
+  mybutton.addEventListener("click", scrollToTop);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  // Show or hide the back to top button based on the scroll position
+  window.addEventListener("scroll", toggleBackToTopButton);
+
+  function toggleBackToTopButton() {
+    if (window.scrollY > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
     }
-  
-    // Add click event listener to the button
-    mybutton.addEventListener("click", topFunction);
-  
-    function topFunction() {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    }
-  });  
+  }
+});
